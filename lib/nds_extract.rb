e82686d5df0ece14041836e1ae1ad1ce99305736
Jared Fleming <jaredmf10@gmail.com>
2020-01-21 18:12:45 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,7 +4,7 @@ require 'pp'
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-  
+
     totals = {}
 
     while director_index < director_data.length do
